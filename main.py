f014080afa1aa410cfc46f7510a14b9f7fc398b8
alejandritooo-APR <alejandrodaga@iesmntsia.org>
2025-11-20 17:13:28 +0100

    Primeres linees

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import jocs
+
+print ("Benvingut a la meva maquina arcade, a continuació et mostraré els jocs que tinc disponibles:")
+opcions = input("Tria el num del joc que vols jugar: 1: Jankwn 2: Endivinar num")
