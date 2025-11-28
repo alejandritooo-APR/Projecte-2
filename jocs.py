a69f55e36f7529f69b7a6038e055413629b26a6c
alejandritooo-APR <alejandrodaga@iesmntsia.org>
2025-11-28 15:39:14 +0100

    reformes

--- jocs.py
+++ jocs.py
@@ -1,32 +1,32 @@
-import robot
+from robot import robot
 import random
 
 def janken():
     print("Benvingut a la meva maquina Janken, a continuació et mostrare diferents nombres per poder triar entres les diferents modalitats")
-    nombre = input("Tria un dels següents nombres per poder començar el joc: 1: (Nivell fàcil) És jugaran tres rondes. 2: (Difícl) Al millor de 5 rondes;   ")
-    if nombre == 1:
+    usuari = input("Tria un dels següents nombres per poder començar el joc: 1: (Nivell fàcil) És jugaran tres rondes. 2: (Difícl) Al millor de 5 rondes;   ")
+    if usuari == 1:
         print("Has triat la modalitat fàcil, anem a començar el joc")
-        usuari = input("        Escolleig entre pedra,paper,tisora;  ")
+        usuari = input("Escolleig entre pedra,paper,tisora;  ")
         if usuari == "pedra":
             print("Has triat Pedra")    
         elif usuari == "paper":
             print("Has triat paper")
         elif usuari == "tisora":
             print("Has triat tisora")
-    if usuari == "pedra" and maquina == "tisora":
+    if usuari == "pedra" and robot == "tisora":
         print("Has guanyat")
-    elif usuari == "pedra" and maquina == "paper":      
+    elif usuari == "pedra" and robot == "paper":      
         print("Has perdut")
-    elif usuari == "paper" and maquina == "pedra":
+    elif usuari == "paper" and robot == "pedra":
         print("Has guanyat")
-    elif usuari == "paper" and maquina == "tisora":
+    elif usuari == "paper" and robot == "tisora":
         print("Has perdut")
-    elif usuari == "tisora" and maquina == "paper":
+    elif usuari == "tisora" and robot == "paper":
         print("Has guanyat")
-    elif usuari == "tisora" and maquina == "pedra":
+    elif usuari == "tisora" and robot == "pedra":
         print("Has perdut")  
-         
-    if nombre == 2:
+    robot.playing()  
+    if usuari == 2:
         print("Has triat la modalitat difícil, anem a començar el joc")
         objectes = input("Escolleig entre pedra,paper,tisora;  ")       
         if objectes == "pedra":
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from jocs import janken
 import time
 print ("Benvingut a la meva maquina arcade, a continuació et mostraré els jocs que tinc disponibles:")
-opcions = input("Tria el numerodel joc que vols jugar: 1: Janken 2: Endivinar numero; ")
-if opcions == "1":
+usuari = input("Tria el numerodel joc que vols jugar: 1: Janken 2: Endivinar numero; ")
+if usuari == "1":
     print("El numero seleccionat és 1, per tant jugarem a Janken")
     time.sleep(1)
     janken()
-elif opcions == "2":
+elif usuari == "2":
     print("El numero seleccionat és 2, per tant jugarem a Endivinar numero")
     time.sleep(3)
     endivinar_num()
